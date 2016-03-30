a769c5e8a57c91359b5311381e6d2baebfd329a2
alex <ajkasparek@wpi.edu>
2016-03-30 13:32:35 -0400

    More work on Level_Selection GUI

--- Kabasuji/src/MockupGUI/Level_Selection.java
+++ Kabasuji/src/MockupGUI/Level_Selection.java
@@ -17,15 +17,24 @@ import javax.swing.JProgressBar;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
+import java.awt.Toolkit;
+import java.awt.Button;
+import javax.swing.JButton;
 
 public class Level_Selection {
 
 	private JFrame frame;
 	private JTextField txtLevel;
 	/**
-	 * @wbp.nonvisual location=49,101
+	 * @wbp.nonvisual location=69,171
 	 */
-	private final ImageIcon imageIcon = new ImageIcon();
+	private final ImageIcon Thumbnail = new ImageIcon();
+	private JTextField txtLevel_1;
+	/**
+	 * @wbp.nonvisual location=60,261
+	 */
+	private final ImageIcon Thumbnail2 = new ImageIcon();
+	private JTextField txtLevelSelection;
 
 	/**
 	 * Launch the application.
@@ -54,6 +63,9 @@ public class Level_Selection {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		Thumbnail2.setDescription("Thumbnail?");
+		Thumbnail.setDescription("Thumbnail?");
+		Thumbnail.setImage(Toolkit.getDefaultToolkit().getImage("/Users/alex/Desktop/IQP/CS 3733 Software Engineering/Rhinoceros.jpg"));
 		frame = new JFrame();
 		frame.getContentPane().setFont(new Font("PT Sans Caption", Font.BOLD, 13));
 		frame.getContentPane().setBackground(Color.LIGHT_GRAY);
@@ -63,39 +75,80 @@ public class Level_Selection {
 		scrollBar.setBackground(Color.DARK_GRAY);
 		
 		JProgressBar progressBar = new JProgressBar();
+		progressBar.setMinimum(30);
 		progressBar.setStringPainted(true);
 		progressBar.setForeground(Color.ORANGE);
 		progressBar.setFont(new Font("PT Sans Caption", Font.BOLD, 13));
 		
 		txtLevel = new JTextField();
 		txtLevel.setHorizontalAlignment(SwingConstants.CENTER);
-		txtLevel.setBackground(Color.GRAY);
-		txtLevel.setForeground(Color.DARK_GRAY);
+		txtLevel.setBackground(Color.DARK_GRAY);
+		txtLevel.setForeground(Color.LIGHT_GRAY);
 		txtLevel.setFont(new Font("PT Sans Caption", Font.BOLD, 14));
 		txtLevel.setText("Level 1");
 		txtLevel.setColumns(10);
+		
+		JButton btnPlayLevel = new JButton("Play Level");
+		btnPlayLevel.setBackground(Color.LIGHT_GRAY);
+		btnPlayLevel.setForeground(Color.DARK_GRAY);
+		btnPlayLevel.setFont(new Font("PT Sans Caption", Font.BOLD, 13));
+		
+		txtLevel_1 = new JTextField();
+		txtLevel_1.setText("Level 2");
+		txtLevel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		txtLevel_1.setForeground(Color.LIGHT_GRAY);
+		txtLevel_1.setFont(new Font("PT Sans Caption", Font.BOLD, 14));
+		txtLevel_1.setColumns(10);
+		txtLevel_1.setBackground(Color.DARK_GRAY);
+		
+		JButton button = new JButton("Play Level");
+		button.setForeground(Color.DARK_GRAY);
+		button.setFont(new Font("PT Sans Caption", Font.BOLD, 13));
+		button.setBackground(Color.LIGHT_GRAY);
+		
+		txtLevelSelection = new JTextField();
+		txtLevelSelection.setForeground(Color.LIGHT_GRAY);
+		txtLevelSelection.setHorizontalAlignment(SwingConstants.CENTER);
+		txtLevelSelection.setFont(new Font("PT Sans Caption", Font.BOLD, 18));
+		txtLevelSelection.setText("Level Selection");
+		txtLevelSelection.setBackground(Color.DARK_GRAY);
+		txtLevelSelection.setColumns(10);
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(progressBar, GroupLayout.PREFERRED_SIZE, 411, GroupLayout.PREFERRED_SIZE)
-						.addComponent(txtLevel, GroupLayout.PREFERRED_SIZE, 154, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtLevel_1, GroupLayout.PREFERRED_SIZE, 154, GroupLayout.PREFERRED_SIZE)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(149)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(btnPlayLevel)
+								.addComponent(button, GroupLayout.PREFERRED_SIZE, 109, GroupLayout.PREFERRED_SIZE)))
+						.addComponent(txtLevel, GroupLayout.PREFERRED_SIZE, 154, GroupLayout.PREFERRED_SIZE)
+						.addComponent(progressBar, GroupLayout.PREFERRED_SIZE, 411, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
 					.addComponent(scrollBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+				.addComponent(txtLevelSelection, GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE)
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(6)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+					.addComponent(txtLevelSelection, GroupLayout.PREFERRED_SIZE, 50, GroupLayout.PREFERRED_SIZE)
+					.addGap(34)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(scrollBar, GroupLayout.PREFERRED_SIZE, 215, GroupLayout.PREFERRED_SIZE)
 						.addGroup(groupLayout.createSequentialGroup()
 							.addComponent(txtLevel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(progressBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addComponent(scrollBar, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 266, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(6, Short.MAX_VALUE))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(btnPlayLevel)
+							.addGap(18)
+							.addComponent(txtLevel_1, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(button)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(progressBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
 		);
 		frame.getContentPane().setLayout(groupLayout);
 		frame.setBounds(100, 100, 450, 300);
