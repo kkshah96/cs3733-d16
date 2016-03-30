8dd04eb74d5f1ebd6d98ae6d9ca84afcc3e65b0b
alex <ajkasparek@wpi.edu>
2016-03-29 22:50:00 -0400

    Main menu and level selector menu

---
+++ Kabasuji/src/test/Level_Selection.java
@@ -0,0 +1,104 @@
+package test;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JScrollBar;
+import java.awt.Panel;
+import javax.swing.JMenuItem;
+import javax.swing.JTextField;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Font;
+import java.awt.ScrollPane;
+import javax.swing.JProgressBar;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+
+public class Level_Selection {
+
+	private JFrame frame;
+	private JTextField txtLevel;
+	/**
+	 * @wbp.nonvisual location=49,101
+	 */
+	private final ImageIcon imageIcon = new ImageIcon();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Level_Selection window = new Level_Selection();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Level_Selection() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setFont(new Font("PT Sans Caption", Font.BOLD, 13));
+		frame.getContentPane().setBackground(Color.LIGHT_GRAY);
+		
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setForeground(Color.DARK_GRAY);
+		scrollBar.setBackground(Color.DARK_GRAY);
+		
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setStringPainted(true);
+		progressBar.setForeground(Color.ORANGE);
+		progressBar.setFont(new Font("PT Sans Caption", Font.BOLD, 13));
+		
+		txtLevel = new JTextField();
+		txtLevel.setHorizontalAlignment(SwingConstants.CENTER);
+		txtLevel.setBackground(Color.GRAY);
+		txtLevel.setForeground(Color.DARK_GRAY);
+		txtLevel.setFont(new Font("PT Sans Caption", Font.BOLD, 14));
+		txtLevel.setText("Level 1");
+		txtLevel.setColumns(10);
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(progressBar, GroupLayout.PREFERRED_SIZE, 411, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtLevel, GroupLayout.PREFERRED_SIZE, 154, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addComponent(scrollBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(6)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(txtLevel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(progressBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(scrollBar, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 266, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(6, Short.MAX_VALUE))
+		);
+		frame.getContentPane().setLayout(groupLayout);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
