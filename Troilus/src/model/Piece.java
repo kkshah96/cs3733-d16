d2bfa38e664c7a46b99d12c04e651590a1a5546e
Maddy42 <mclongovt@gmail.com>
2016-04-15 15:16:26 -0400

    Added checking in Board.addPiece, fixed numerous FOR loops

--- Troilus/src/model/Piece.java
+++ Troilus/src/model/Piece.java
@@ -2,28 +2,31 @@ package model;
 
 // TODO: Should we make Piece subclasses for level types so square types are consistent?
 public class Piece {
-
 	Square[] squares;
 	Square anchor;
-	int row, col; // TODO: Do we need this? If so, update constructor(s)
+	//int row, col; // TODO: Do we need this? If so, update constructor(s)
 
+	// TODO: Make sure we check that the Square array is an appropriate size at execution time (throw exception if it isn't?)
 	public Piece(Square[] squares, Square anchor) {
 		this.squares = squares;
 		this.anchor = anchor;
-		this.row = 0;
-		this.col = 0;
 	}
 	
-	// TODO: Make sure we check that the Square array is an appropriate size at execution time (throw exception if it isn't?)
-	public Piece(Square[] squares, Square anchor, int row, int col) {
-		this.squares = squares;
-		this.anchor = anchor;
-		this.row = row;
-		this.col = col;
+	public int getRow() {
+		return anchor.row;
 	}
 	
+	public int getCol() {
+		return anchor.col;
+	}
+	
+	/** Update location of anchor
+	 * @param row
+	 * @param col
+	 * @return
+	 */
 	public Piece place(int row, int col) {
-		return new Piece(squares, anchor, row, col);
+		return new Piece(squares, new PieceSquare(row, col));
 	}
 	
 	public Piece rotatePiece(int degree){ //CCW: degree < 0
@@ -55,7 +58,7 @@ public class Piece {
 		Square[] newSquares = new Square[5];
 
 		for(int i = 0; i < 4; i++){
-			if(degree < 0){ //FLIP HORIZONTAL
+			if(degree < 0){ // FLIP HORIZONTAL
 				int tempRow;
 				int tempCol;
 				tempRow = squares[i].getRow();
