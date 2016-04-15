d529b29d131c687ee1d870f350f8c28aa6061654
alex <ajkasparek@wpi.edu>
2016-04-15 00:57:26 -0400

    Added logic to rotate piece CCW and CW (but commented it out because square is abstract -- but the logic is sound I'm pretty sure)

--- Troilus/src/model/Piece.java
+++ Troilus/src/model/Piece.java
@@ -13,8 +13,32 @@ public class Piece {
 		this.anchor = anchor;
 	}
 	
-	public Piece rotatePiece(int degree){
-		return new Piece(squares, anchor); // TODO: Implement this
+	public Piece rotatePiece(int degree){ //CCW: degree < 0
+		Square[] newSquares = new Square[5];
+		
+		
+		for(int i = 0; i < 4; i++){
+//			if(degree < 0){ //ROTATE CCW
+//				int tempRow;
+//				int tempCol;
+//				tempRow = squares[i].getRow();
+//				tempCol = squares[i].getCol();
+//				Square newSquare = new PieceSquare( tempCol, tempRow);
+//			}
+			
+//			if(degree >= 0){ //ROTATE CW
+//				int tempRow;
+//				int tempCol;
+//				tempRow = squares[i].getRow();
+//				tempCol = squares[i].getCol();
+//				Square newSquare = new PieceSquare(tempCol, -1*tempRow);
+//			}
+			
+			
+			
+		}
+		
+		return new Piece(newSquares, anchor); // TODO: Implement this
 	}
 	
 	public Piece flipPiece(int degree){
