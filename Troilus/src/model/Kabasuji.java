45314bfd15644af6211f2463846a70bd7722cdc4
Connor Weeks <cw@bluespiral.io>
2016-04-25 12:47:37 -0400

    Kabasuji now loads levels correctly.

--- Troilus/src/model/Kabasuji.java
+++ Troilus/src/model/Kabasuji.java
@@ -19,36 +19,35 @@ public class Kabasuji {
 	
 	public Kabasuji() {
 		
-		// TODO: UNCOMMENT THIS LATER- fix unlocking from stars of prior level
-		/*
 		// create level loader controller
 		LoadLevelsController loader = new LoadLevelsController();
-		
+
 		// load levels into the list of levels
 		levels = loader.loadLevels();
-		*/
+		
+		
 		// stubs for testing:
 		
-		levels = new ArrayList<Level>();
-		for(int i = 0; i < 24; i++){
-			Level level;
-			if (i % 3 == 0) {
-				level = new PuzzleLevel(i, false, new Bullpen(), new Board(), new  Palette(), 10);
-			}
-			else if (i % 3 == 1) {
-				level = new LightningLevel(i, false, new Bullpen(), new Board(), new  Palette(), 10);
-			}
-			else {
-				level = new ReleaseLevel(i, false, new Bullpen(), new Board(), new  Palette());
-			}
-			
-			// show only the first level as unlocked, lock every other level
-			if(i >= 1) {
-				level.setLocked(true);
-			}
-			
-			levels.add(level);
-		}
+//		levels = new ArrayList<Level>();
+//		for(int i = 0; i < 24; i++){
+//			Level level;
+//			if (i % 3 == 0) {
+//				level = new PuzzleLevel(i, false, new Bullpen(), new Board(), new  Palette(), 10);
+//			}
+//			else if (i % 3 == 1) {
+//				level = new LightningLevel(i, false, new Bullpen(), new Board(), new  Palette(), 10);
+//			}
+//			else {
+//				level = new ReleaseLevel(i, false, new Bullpen(), new Board(), new  Palette());
+//			}
+//			
+//			// show only the first level as unlocked, lock every other level
+//			if(i >= 1) {
+//				level.setLocked(true);
+//			}
+//			
+//			levels.add(level);
+//		}
 		
 	}
 
