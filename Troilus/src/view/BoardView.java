55bc81d0c980fddb2cd5cd30cb9ceaa5c3239bf1
Kunal Shah <kkshah@wpi.edu>
2016-04-25 15:07:42 -0400

    Sort of got pieces drawing correctly on the BoardView

--- Troilus/src/view/BoardView.java
+++ Troilus/src/view/BoardView.java
@@ -97,7 +97,11 @@ public class BoardView extends JPanel {
 		if(board.getDraggedPiece() != null) {
 			System.out.println("Painting dragged piece");
 			System.out.println("Point " + board.getDraggedPiecePoint());
-			pDrawer.paint(g, board.getDraggedPiece(), board.getDraggedPiecePoint(), SQUARE_SIZE, 0, 0);
+			
+			Point p = board.getDraggedPiecePoint();
+			int x = p.x / SQUARE_SIZE;
+			int y = p.y / SQUARE_SIZE;
+			pDrawer.paint(g, board.getDraggedPiece(), new Point(x, y), SQUARE_SIZE, HEIGHT_OFFSET, WIDTH_OFFSET);
 		}
 		
 	}
