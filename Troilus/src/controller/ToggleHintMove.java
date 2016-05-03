c2397aa720eff83e4ee1d848ba031d4c7d7f38cb
Connor Weeks <cw@bluespiral.io>
2016-05-03 01:49:27 -0400

    Added comments for toggle hint move.

--- Troilus/src/controller/ToggleHintMove.java
+++ Troilus/src/controller/ToggleHintMove.java
@@ -5,25 +5,38 @@ import model.Square;
 
 
 /**
+ * Class for implementing toggling a hint in the level builder.
+ * This move class stores the row and column of the square being
+ * toggled, and may use this information to undo and redo actions.
  * 
  * @author Connor Weeks
  *
  */
-public class ToggleHintMove extends Move{
+public class ToggleHintMove extends Move {
+	
+	/** The square being toggled */
 	Square toggledSquare;
+	
+	/** The level being edited */
 	Level level;
-	//Square activeSquare;
 
-	int col;
-	int row;
-
-	//TODO: Still need to finish logic for undoing and redoing....
+	/**
+	 * Constructor for ToggleHintMove.
+	 * 
+	 * @param toggledSquare The square being toggled.
+	 * @param level The level currently being edited.
+	 */
 	public ToggleHintMove(Square toggledSquare, Level level){
 		super();
 		this.toggledSquare = toggledSquare;
 		this.level = level;
-		//this.activeSquare = level.getBoard().getActiveSquare();
 	}
+	
+	/**
+	 * Sets the active square to the square being toggled and toggle the hint for that square.
+	 * 
+	 * @return True if move is valid; false otherwise;
+	 */
 	@Override
 	public boolean doMove() {
 		boolean validation = false;
@@ -38,15 +51,18 @@ public class ToggleHintMove extends Move{
 		return validation;
 	}
 
+	/**
+	 * Returns true if the boards active square is not null and it is valid.
+	 */
 	@Override
 	public boolean isValid() {
-		boolean validation = false;
-		if(level.getBoard().getActiveSquare() != null && level.getBoard().getActiveSquare().isValid()){
-			validation = true;
-		}
-		return validation;
+		return (level.getBoard().getActiveSquare() != null && level.getBoard().getActiveSquare().isValid());
 	}
 
+	/**
+	 * Sets the active square to the square being toggled and performs the toggle operation again,
+	 * returning the hint of that square to its initial state.
+	 */
 	@Override
 	public boolean undo() {
 		level.getBoard().setActiveSquare(toggledSquare.getCol(), toggledSquare.getRow());
